00a47c669674c82e47dddc33c79b5a374c8dacdb
makroterno <kagan.akar1@gmail.com>
2024-04-23 00:07:45 +0300

    optimization

--- scripts/tilemap.py
+++ scripts/tilemap.py
@@ -12,8 +12,8 @@ class Tilemap:
         self.offgrid_tiles = []
 
         for i in range(10):
-            self.tilemap[str(3+i) + ';10'] = {'type': 'grass', 'variant': 0, 'pos': (3 + i, 10)}
-            self.tilemap['10;' + str(5+i)] = {'type': 'dirt', 'variant': 0, 'pos': (10, 5 + i)}
+            self.tilemap[str(3 + i) + ';10'] = {'type': 'grass', 'variant': 0, 'pos': (3 + i, 10)}
+            self.tilemap['10;' + str(5 + i)] = {'type': 'dirt', 'variant': 0, 'pos': (10, 5 + i)}
 
     def tiles_around(self, pos):
         tiles = []
@@ -40,7 +40,12 @@ class Tilemap:
             surf.blit(self.game.assets[tile['type']][tile['variant']],
                       (tile['pos'][0] - offset[0], tile['pos'][1] - offset[1]))
 
-        for loc in self.tilemap:
-            tile = self.tilemap[loc]
-            surf.blit(self.game.assets[tile['type']][tile['variant']],
-                      ((tile['pos'][0] * self.tile_size) - offset[0], (tile['pos'][1] * self.tile_size) - offset[1]))
+        for x in range(offset[0] // self.tile_size, (offset[0] + surf.get_width()) // self.tile_size + 1):
+            for y in range(offset[1] // self.tile_size, (offset[1] + surf.get_height()) // self.tile_size + 1):
+                loc = str(x) + ";" + str(y)
+                if loc in self.tilemap:
+                    tile = self.tilemap[loc]
+                    surf.blit(self.game.assets[tile['type']][tile['variant']],
+                              ((tile['pos'][0] * self.tile_size) - offset[0],
+                               (tile['pos'][1] * self.tile_size) - offset[1]))
+
